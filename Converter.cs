8c6cbe41d651a5d99e58b71edf48c1d87d157fe2
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-09-03 09:56:14 +1200

    Added separate file for the conversion classes

---
+++ Converter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_converter
+{
+    class Converter
+    {
+        public static string Reverse(string binary)
+        {
+            char[] array = binary.ToCharArray();
+            string reverse = string.Empty;
+            for (int i = array.Length - 1; i > -1; i--)
+            {
+                reverse += array[i];
+            }
+            return reverse;
+        }
+
+        public static long BinaryToDecimal(string binary)
+        {
+            long factor = 1;
+            long decimal_ = 0;
+
+            for (int i = binary.Length - 1; i >= 0; i--)
+            {
+                if (binary[i].ToString() == "1") decimal_ += factor;
+                factor *= 2;
+            }
+            return decimal_;
+        }
+
+        public static string DecimalToBinary(long decimal_)
+        {
+            string binary = string.Empty;
+
+            while (decimal_ > 0)
+            {
+                if (decimal_ % 2 == 1)
+                {
+                    decimal_ = (decimal_ - 1) / 2;
+                    binary += "1";
+                }
+                else
+                {
+                    decimal_ /= 2;
+                    binary += "0";
+                }
+            }
+
+            return Reverse(binary);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,59 +17,14 @@ namespace Binary_converter
             {
                 Console.Write("\nEnter a binary number: ");
                 string binary = Console.ReadLine();
-                Console.WriteLine(BinaryToDecimal(binary).ToString());
+                Console.WriteLine(Converter.BinaryToDecimal(binary).ToString());
             }
             while (choice == '2')
             {
                 Console.Write("\nEnter a decimal number: ");
                 long decimal_ = Convert.ToInt64(Console.ReadLine());
-                Console.WriteLine(DecimalToBinary(decimal_));
+                Console.WriteLine(Converter.DecimalToBinary(decimal_));
             }
-        }
-
-        public static string Reverse(string binary)
-        {
-            char[] array = binary.ToCharArray();
-            string reverse = string.Empty;
-            for (long i = array.Length - 1; i > -1; i--)
-            {
-                reverse += array[i];
-            }
-            return reverse;
-        }
-
-        public static long BinaryToDecimal(string binary)
-        {
-            long factor = 1;
-            long decimal_ = 0;
-
-            for (int i = binary.Length - 1; i >= 0; i--)
-            {
-                if (binary[i].ToString() == "1") decimal_ += factor;
-                factor *= 2;
-            }
-            return decimal_;
-        }
-
-        public static string DecimalToBinary(long decimal_)
-        {
-            string binary = string.Empty;
-
-            while (decimal_ > 0)
-            {
-                if (decimal_ % 2 == 1)
-                {
-                    decimal_ = (decimal_ - 1) / 2;
-                    binary += "1";
-                }
-                else
-                {
-                    decimal_ /= 2;
-                    binary += "0";
-                }
-            }         
-
-            return Reverse(binary);
         }
     }
 }
